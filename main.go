537d787142deab16818b5026a63f250efb86ccb6
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2023-05-15 11:50:05 -0300

    adiconado cookies

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"codigo-fluente/database"
 	"codigo-fluente/routes"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,6 +11,10 @@ import (
 func main() {
 	database.Connect()
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
 	routes.Setup(app)
 	app.Listen(":3000")
 
